fd49e6adc21f630db8da0b3ff8cc9e6c3dbc1e53
Abhishek <abhishek.bhattacharya@gmail.com>
2019-05-15 16:02:03 +0530

    Initial commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Layout from '../components/layout';
+
+const About = () =>(
+    <Layout>
+    <div>
+        <h1>About BitzPrice</h1>
+        <p>Application for viewing to Bitzprice</p>
+        </div>
+        </Layout>
+    );
+    
+    export default About;
